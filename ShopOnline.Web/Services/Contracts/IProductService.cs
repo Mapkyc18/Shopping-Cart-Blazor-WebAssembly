dd0f7b42e62453393b3e7d55e70acaf7960bf311
Marcus Galdino <marcusgaldino3@gmail.com>
2024-12-04 21:42:52 -0700

    ProductService class created and On the Try Blcok Gotta fix return obj.

--- ShopOnline.Web/Services/Contracts/IProductService.cs
+++ ShopOnline.Web/Services/Contracts/IProductService.cs
@@ -5,8 +5,5 @@ namespace ShopOnline.Web.Services.Contracts
     public interface IProductService
     {
         Task<IEnumerable<ProductDto>> GetItems();
-        Task<ProductDto> GetItem(int id);
-        Task<IEnumerable<ProductCategoryDto>> GetProductCategories();
-        Task<IEnumerable<ProductDto>> GetItemsByCategory(int categoryId);
     }
 }
---
+++ ShopOnline.Web/Services/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using ShopOnline.Web.Services.Contracts;
+using System.Net.Http.Json;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public async Task<IEnumerable<ProductDto>> GetItems()
+        {
+            try
+            {
+                var response = await httpClient.GetFromJsonAsync<IEnumerable<ProductDto>>("api/Product");
+                return default(ProductDto);
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
