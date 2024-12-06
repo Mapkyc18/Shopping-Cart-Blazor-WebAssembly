77a4d6abd4967f540b7cc7127ed932039868efc1
Marcus Galdino <marcusgaldino3@gmail.com>
2024-12-06 00:01:04 -0700

    GetCategory method changed on ProductRepository.cs.

--- ShopOnline.Api/Controllers/ProductController.cs
+++ ShopOnline.Api/Controllers/ProductController.cs
@@ -44,5 +44,33 @@ namespace ShopOnline.Api.Controllers
                                 "Error retrieving data from the database");
             }
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ProductDto>> GetItem(int id)
+        {
+            try
+            {
+                var product = await this.productRepository.GetItem(id);
+
+                if (product == null)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+
+                    var productDto = product.ConvertToDto();
+
+                    return Ok(productDto);
+                }
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                "Error retrieving data from the database");
+
+            }
+        }
     }
 }
