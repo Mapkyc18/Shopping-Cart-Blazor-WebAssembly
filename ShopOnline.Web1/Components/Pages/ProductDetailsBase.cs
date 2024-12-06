955383f683c00d634129e8e75e68c8dffe62d3c1
Marcus Galdino <marcusgaldino3@gmail.com>
2024-12-06 00:26:43 -0700

    ProductDetailsBase Code Implemented.

--- ShopOnline.Web1/Components/Pages/ProductDetailsBase.cs
+++ ShopOnline.Web1/Components/Pages/ProductDetailsBase.cs
@@ -1,6 +1,32 @@
-﻿namespace ShopOnline.Web1.Components.Pages
+﻿using Microsoft.AspNetCore.Components;
+using ShopOnline.Models.Dtos;
+using ShopOnline.Web1.Services.Contracts;
+
+namespace ShopOnline.Web1.Components.Pages
 {
-    public class ProductDetailsBase
+    public class ProductDetailsBase : ComponentBase
     {
+        [Parameter]
+        public int Id { get; set; }
+
+        [Inject]
+        public IProductService ProductService { get; set; }
+
+        public ProductDto Product { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                Product = await ProductService.GetItem(Id);
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+        }
+
     }
 }
